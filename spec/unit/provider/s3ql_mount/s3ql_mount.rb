230326224da931d3060012164e08e71a3154a7a7
Igor Galić <i.galic@brainsware.org>
2015-09-19 18:56:12 +0200

    add a simple spec test for the provider

---
+++ spec/unit/provider/s3ql_mount/s3ql_mount.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+type_class     = Puppet::Type.type(:s3ql_mount)
+provider_class = type_class.provide(:s3ql_mount)
+
+describe provider_class do
+
+  context 'with the minimum parameters' do
+    let(:resource) {
+      type_class.new(
+        mountpoint: '/mnt',
+        storage_url: 'gs://bucket/prefix',
+        owner: 'examplewww',
+        group: 'examplewww',
+      )
+    }
+
+    let(:provider) { resource.provider }
+    let(:instance) { provider.class.instance.first }
+
+    it 'should be an instance of the S3ql_mount' do
+      expect(provider).to be_an_instance_of Puppet::Type::S3ql_mount::S3ql_mount
+    end
+  end
+end
