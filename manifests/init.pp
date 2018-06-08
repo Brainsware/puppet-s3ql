2a181db993d8a4742598c083d9797e4e9bc05e04
Igor Galić <igalic@brainsware.at>
2018-06-08 10:30:55 +0200

    add allow_other to s3ql init class
    when allow other is set, we ensure that the option "user_allow_other"
    option is correctly set in `/etc/fuse.conf`

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,11 +7,20 @@ class s3ql (
   $package_name     = 's3ql',
   $package_ensure   = 'present',
   $package_provider = undef,
+  $allow_other = false,
 ) {
 
   package { 's3ql':
     ensure   => $package_ensure,
     name     => $package_name,
     provider => $package_provider,
+  }
+
+  if $allow_other {
+    file_line { "user_allow_other ${allow_other}":
+      path  => '/etc/fuse.conf',
+      line  => 'user_allow_other',
+      match => '^#user_allow_other'
+    }
   }
 }
